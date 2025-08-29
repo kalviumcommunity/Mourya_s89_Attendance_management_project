216745df3498a3d50a73785b62f0fd73cacc0cc2
mourya-s89 <kadali.mourya.s89@kalvium.community>
2025-08-29 09:48:44 +0530

    attendancerecord

---
+++ src/com/school/AttendanceRecord.java
@@ -0,0 +1,39 @@
+package com.school;
+
+public class AttendanceRecord {
+    private int studentId;
+    private int courseId;
+    private String status;
+
+    public AttendanceRecord(int studentId, int courseId, String status) {
+        this.studentId = studentId;
+        this.courseId = courseId;
+
+        // Validation
+        if (status.equalsIgnoreCase("Present") || status.equalsIgnoreCase("Absent")) {
+            this.status = status.substring(0,1).toUpperCase() + status.substring(1).toLowerCase(); 
+        } else {
+            this.status = "Invalid";
+            System.out.println("⚠️ Warning: Invalid attendance status entered. Defaulted to 'Invalid'.");
+        }
+    }
+
+    // Getters
+    public int getStudentId() {
+        return studentId;
+    }
+
+    public int getCourseId() {
+        return courseId;
+    }
+
+    public String getStatus() {
+        return status;
+    }
+
+    public void displayRecord() {
+        System.out.println("AttendanceRecord => Student ID: " + studentId + 
+                           ", Course ID: " + courseId + 
+                           ", Status: " + status);
+    }
+}
--- src/com/school/Student.java
+++ src/com/school/Student.java
@@ -1,16 +1,25 @@
 package com.school;
 
 public class Student {
-    private static int nextStudentIdCounter = 1;
-    int studentId;
-    String name;
+    private int studentId;
+    private String name;
+    private static int idCounter = 1;
 
     public Student(String name) {
-        this.studentId = nextStudentIdCounter++;
         this.name = name;
+        this.studentId = idCounter++;
     }
 
-    public void displayDetails() {
-        System.out.println("Student ID: " + this.studentId + ", Name: " + this.name);
-}
+    // Getters
+    public int getStudentId() {
+        return studentId;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void displayStudent() {
+        System.out.println("Student ID: " + studentId + ", Name: " + name);
+    }
 }
